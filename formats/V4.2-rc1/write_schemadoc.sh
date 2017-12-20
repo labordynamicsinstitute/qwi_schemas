4e40fce5c4be1dfdfed1fb67e94e051bdeac87e1
Lars Vilhuber <lars.vilhuber@cornell.edu>
2017-12-20 18:24:58 -0500

    Incorporated a fix from master a86dcd2 into script

--- formats/V4.2-rc1/write_schemadoc.sh
+++ formats/V4.2-rc1/write_schemadoc.sh
@@ -662,7 +662,7 @@ include::tmp_$arg[]
 |===================================================
 
 For instance, the metadata for the $versionvintage release of
-$(grep "$versionstate," naming_geohi.csv | awk  -F, ' { print $2 } ' ) QWI
+$(grep -E "^$versionstate," naming_geohi.csv | awk  -F, ' { print $2 } ' | sed 's/"//g') QWI
 $(grep "$versiondemo," naming_demo.csv | awk  -F, ' { print $2 } ' )
 for $(grep "$versionfas," naming_fas.csv | awk  -F, ' { print $2 } ')
 (obtained from $versionurl/version_${versiondemo}_${versionfas}.txt[here]) has  the following content:
@@ -670,7 +670,7 @@ for $(grep "$versionfas," naming_fas.csv | awk  -F, ' { print $2 } ')
 $(curl $versionurl/version_${versiondemo}_${versionfas}.txt)
 --------------------------------
 Similarly, the metadata for the $versionvintage release of
-$(grep "$versionstate," naming_geohi.csv | awk  -F, ' { print $2 } ' ) J2J
+$(grep -E "^$versionstate," naming_geohi.csv | awk  -F, ' { print $2 } ' | sed 's/"//g') J2J
 tabulations (obtained from $versionj2jurl/version_j2j.txt[here]) has  the following content:
 --------------------------------
 $(curl $versionj2jurl/version_j2j.txt)
