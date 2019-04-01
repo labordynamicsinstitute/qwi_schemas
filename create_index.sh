91b86316b21777d84aa2e10ff2d2257b41ac9eef
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-01 14:15:46 +0000

    Deploy labordynamicsinstitute/qwi_schemas to github.com/labordynamicsinstitute/qwi_schemas.git:gh-pages

--- create_index.sh
+++ create_index.sh
@@ -2,17 +2,27 @@
 if [[ -z $1 ]]
 	then
 	cat << EOF
-	$0 start
+	$0 [branch]
 
 	will generate a index.html page from fragments for gh-pages.
 
 	Requires: multimarkdown
+
+	Does not run on lehd-related  branches. 
 EOF
 exit 0
 fi
 
-cat header.frag > index.html
-multimarkdown README.md >> index.html
-cat footer.frag >> index.html
-git add index.html
-git status
+wd=$(dirname $0)
+echo $wd
+
+case $1 in
+   lehd*)
+   echo "does not run for branch $1"
+   ;;
+   *)
+	cat $wd/header.frag | sed "s/BUILDDATE/$(date)/" | sed "s/BUILDINFO/$1/"> $wd/index.html
+	multimarkdown $wd/README.md >> $wd/index.html
+	cat $wd/footer.frag >> $wd/index.html
+	;;
+esac
