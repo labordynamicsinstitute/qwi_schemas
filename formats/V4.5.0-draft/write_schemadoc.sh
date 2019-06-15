38ebfb89887fa05f82760ce0bec04e57f7bde5b7
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2019-06-14 20:30:48 -0400

    fixes #131

--- formats/V4.5.0-draft/write_schemadoc.sh
+++ formats/V4.5.0-draft/write_schemadoc.sh
@@ -846,6 +846,10 @@ include::$arg[]
 
 Graduate counts are provided from public use data from the https://nces.ed.gov/ipeds/use-the-data[Integrated Postsecondary Education Data System (IPEDS)]. Counts are linked to graduation cohorts in the PSEO data and included in the PSEOE tables. In a small number of cases, misalignment in programs (CIPCODE) is observed between the IPEDS and PSEO counts. In these cases, the IPEDS counts adjusted to be consistent with those on PSEO, and the count is flagged accordingly.
 
+[width=\"80%\",format=\"csv\",cols=\"^1,<4\",options=\"header\"]
+|===================================================
+include::label_flags_ipeds_count.csv[]
+|===================================================
 
 
 ">> $asciifile
